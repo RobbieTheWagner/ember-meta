394de92c4e662914fa8fa7f768e6f3c6152be706
Robert Wagner <rwwagner90@gmail.com>
2018-04-15 09:44:00 -0400

    Use config

--- addon/mixins/blog-meta.js
+++ addon/mixins/blog-meta.js
@@ -1,4 +1,25 @@
 import Mixin from '@ember/object/mixin';
+import { assert } from '@ember/debug';
+import { inject as service } from '@ember/service';
+import config from 'ember-get-config';
 
 export default Mixin.create({
+  headData: service(),
+
+  afterModel() {
+    this._super(...arguments);
+
+    const premberMetaConfig = config['prember-meta'];
+
+    assert(`The blog-meta mixin requires values to be set in config['prember-meta'].`, premberMetaConfig !== undefined);
+
+    const {description, title, url} = premberMetaConfig;
+
+    return setProperties(this.get('headData'), {
+      description,
+      title,
+      url,
+      type: 'website'
+    });
+  }
 });
--- addon/mixins/post-meta.js
+++ addon/mixins/post-meta.js
@@ -1,4 +1,32 @@
 import Mixin from '@ember/object/mixin';
+import { assert } from '@ember/debug';
+import { inject as service } from '@ember/service';
+import config from 'ember-get-config';
 
 export default Mixin.create({
+  headData: service(),
+
+  afterModel(model) {
+    this._super(...arguments);
+
+    const premberMetaConfig = config['prember-meta'];
+
+    assert('The post-meta mixin requires a model to be defined.', model !== undefined);
+    assert(`The post-meta mixin requires values to be set in config['prember-meta'].`, premberMetaConfig !== undefined);
+
+    const description = `${model.content.substring(0, 260)}...`;
+    const { author, categories, date, slug, title, type } = model.attributes;
+
+    return setProperties(this.get('headData'), {
+      title: `${title} - ${premberMetaConfig.title}`,
+      articleTitle: title,
+      author,
+      description,
+      date,
+      keywords: categories.join(', '),
+      tags: categories,
+      type: type || 'article',
+      url: `${premberMetaConfig.url}${slug}/`
+    });
+  }
 });
