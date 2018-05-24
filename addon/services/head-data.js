32ef67deb6e6cf76980e84c3ad78f48089d2a86a
Chris Manson <chris@stonecircle.io>
2018-05-24 15:56:00 +0100

    removing attributes prefix

--- addon/services/head-data.js
+++ addon/services/head-data.js
@@ -19,19 +19,19 @@ export default Service.extend({
    * Used for og:title, twitter:title as the title to show in the unfurled links
    */
   articleTitle: computed('routeName', function() {
-    return this.get('currentRouteModel.attributes.articleTitle');
+    return this.get('currentRouteModel.articleTitle');
   }),
   /**
    * Used for twitter 'written by' meta.
    */
   author: computed('routeName', function() {
-    return this.get('currentRouteModel.attributes.author');
+    return this.get('currentRouteModel.author');
   }),
   /**
    * Used for article:published_time
    */
   date: computed('routeName', function() {
-    return this.get('currentRouteModel.attributes.date');
+    return this.get('currentRouteModel.date');
   }),
   /**
    * Used for <meta name="description">, og:description, twitter:description
@@ -47,10 +47,10 @@ export default Service.extend({
     return this.getWithDefault('currentRouteModel.attributes.description', emberMetaConfig.description);
   }),
   /**
-   * Not used directly - used by keywords & tags
+   * Internal - used by keywords & tags
    */
   categories: computed('routeName', function() {
-    return this.get('currentRouteModel.attributes.categories');
+    return this.get('currentRouteModel.categories');
   }),
   /**
    * Used for twitter meta to display 'filed under'
@@ -63,13 +63,13 @@ export default Service.extend({
    * Used for og:image twitter:image:src, the image to display in your unfurled links
    */
   imgSrc: computed('routeName', function() {
-    return this.getWithDefault('currentRouteModel.attributes.imgSrc', emberMetaConfig.imgSrc);
+    return this.getWithDefault('currentRouteModel.imgSrc', emberMetaConfig.imgSrc);
   }),
   /**
    * Used for og:site_name
    */
   siteName: computed('routeName', function() {
-    return this.getWithDefault('currentRouteModel.attributes.siteName', emberMetaConfig.siteName);
+    return this.getWithDefault('currentRouteModel.siteName', emberMetaConfig.siteName);
   }),
   /**
    * Used for article:tag
@@ -81,26 +81,26 @@ export default Service.extend({
    * Used for <title>, og:title, twitter:title
    */
   title: computed('routeName', function() {
-    return this.getWithDefault('currentRouteModel.attributes.title', emberMetaConfig.title);
+    return this.getWithDefault('currentRouteModel.title', emberMetaConfig.title);
   }),
   /**
    * Used for twitter:site and twitter:creator
    */
   twitterUsername: computed('routeName', function() {
-    return this.getWithDefault('currentRouteModel.attributes.twitterUsername', emberMetaConfig.twitterUsername);
+    return this.getWithDefault('currentRouteModel.twitterUsername', emberMetaConfig.twitterUsername);
   }),
   /**
    * Used for og:type, defaults to 'website'
    */
   type: computed('routeName', function() {
-    return this.getWithDefault('currentRouteModel.attributes.type', 'website');
+    return this.getWithDefault('currentRouteModel.type', 'website');
   }),
   /**
    * Used for <link rel="canonical">, og:url, twitter:url
    */
   url: computed('routeName', function() {
-    let url = this.getWithDefault('currentRouteModel.attributes.url', emberMetaConfig.url);
-    const slug = this.get('currentRouteModel.attributes.slug');
+    let url = this.getWithDefault('currentRouteModel.url', emberMetaConfig.url);
+    const slug = this.get('slug');
     if (slug) {
       url = `${url}${slug}/`;
     }
@@ -111,5 +111,11 @@ export default Service.extend({
    */
   canonical: computed('routeName', function() {
     return this.get('currentRouteModel.attributes.canonical');
-  })
+  }),
+  /**
+   * Internal - used for url
+   */
+  slug: computed('routeName', function() {
+    return this.get('currentRouteModel.slug');
+  }),
 });
