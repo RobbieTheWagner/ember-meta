7c7ed553a382c8f3beb7dcc31ffa4ab837d076bf
Chris Manson <chris@stonecircle.io>
2018-05-24 17:29:07 +0100

    fixing tests

--- tests/dummy/app/routes/blog/post.js
+++ tests/dummy/app/routes/blog/post.js
@@ -4,15 +4,13 @@ export default Route.extend({
   model() {
     return {
       content: '<h1>Overridden post content</h1> <p>This is a post body!</p>',
-      attributes: {
-        author: 'Robert Wagner',
-        authorId: 'rwwagner90',
-        categories: ['ember', 'ember.js'],
-        date: '2018-04-09',
-        slug: 'test-post-slug',
-        title: 'Overridden Title',
-        type: 'article'
-      }
+      author: 'Robert Wagner',
+      authorId: 'rwwagner90',
+      categories: ['ember', 'ember.js'],
+      date: '2018-04-09',
+      slug: 'test-post-slug',
+      title: 'Overridden Title',
+      type: 'article'
     };
   }
 });
